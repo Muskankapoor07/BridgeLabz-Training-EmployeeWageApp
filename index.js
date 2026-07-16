9c12dbd1e71083fff37119e35737fb3b92aaa9f9
Muskan Kapoor <kapoormuskan700@gmail.com>
2026-07-16 23:50:48 +0530

    Add:Refactored code to add getWorkHours function for UC3

--- index.js
+++ index.js
@@ -5,37 +5,42 @@ const PART_TIME = 1;
 const FULL_TIME = 2;
 const WAGE_PER_HOUR = 20;
 
+function getWorkHours() {
+    let empCheck = Math.floor(Math.random() * 3);
+
+    switch (empCheck) {
+        case PART_TIME:
+            return 4;
+
+        case FULL_TIME:
+            return 8;
+
+        default:
+            return 0;
+    }
+}
+
 function calculateDailyWage() {
 
     let employeeCheck = Math.floor(Math.random() * 2);
 
     if (employeeCheck == IS_PRESENT) {
+
         console.log("Employee is Present");
 
-        let empCheck = Math.floor(Math.random() * 2) + 1;
-        let empHours = 0;
-
-        switch (empCheck) {
-            case PART_TIME:
-                empHours = 4;
-                console.log("Part Time Employee");
-                break;
-
-            case FULL_TIME:
-                empHours = 8;
-                console.log("Full Time Employee");
-                break;
-        }
-
+        let empHours = getWorkHours();
         let empWage = empHours * WAGE_PER_HOUR;
+
         console.log("Employee Hours = " + empHours);
         console.log("Daily Employee Wage = $" + empWage);
 
     } else {
+
         console.log("Employee is Absent");
         console.log("Employee Hours = 0");
         console.log("Daily Employee Wage = $0");
     }
 }
+
 
 calculateDailyWage();
