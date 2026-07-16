3fdfaa3421d0cd699aec974d4ee0ac2dc23ad383
Muskan Kapoor <kapoormuskan700@gmail.com>
2026-07-16 23:25:06 +0530

    Add: UC1 Check Employee Attendance

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log("EmployeeWage Project started");
+
+const IS_PRESENT = 1;
+let employeeCheck = Math.floor(Math.random() * 2);
+
+if (employeeCheck === IS_PRESENT) {
+    console.log("Employee is Present");
+} else {
+    console.log("Employee is Absent");
+}
